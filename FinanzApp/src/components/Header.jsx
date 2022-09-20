e38e53a2f2f616d32d6dd4d84bfb9b8677e709c7
Juan Daniel Valencia <112526775+nielvadev@users.noreply.github.com>
2022-09-19 19:38:18 -0500

    Agregación Componente Mensaje

--- FinanzApp/src/components/Header.jsx
+++ FinanzApp/src/components/Header.jsx
@@ -1,26 +1,33 @@
 import NuevoPresupuesto from "./NuevoPresupuesto"
 
-const Header = () => {
+const Header = ({
+    presupuesto, 
+    setPresupuesto, 
+    isValidPresupuesto, 
+    setIsValidPresupuesto}) => {
 
-  return (
-    <>
+    return (
         <header>
             <a href="localhost"><img id="logo" src="src\img\FinanzAppB.png"></img></a>
             <a href="https://github.com/nielvadev"><img id="git" src="src\img\gitLogo.png"></img></a>
-            <h1>planificador de gastos</h1>
-        </header>
+            <h1>Planificador de gastos</h1>
+
         <nav class="barra">
             <button id="menu">Presupuesto</button>
             <button id="menu">Inicio</button>
             <button id="menu">Balances</button>
             
         </nav>
-        <header>
-            <NuevoPresupuesto />
+            {isValidPresupuesto ? (
+                <p>Control Presupuesto</p>
+            ):(
+                <NuevoPresupuesto 
+                    presupuesto = {presupuesto}
+                    setPresupuesto = {setPresupuesto}
+                    setIsValidPresupuesto = {setIsValidPresupuesto}
+                />
+            )}
         </header>
-       
-            
-    </>
-  )
+    )
 }
 export default Header
--- FinanzApp/src/components/NuevoPresupuesto.jsx
+++ FinanzApp/src/components/NuevoPresupuesto.jsx
@@ -1,22 +1,47 @@
-const NuevoPresupuesto = () => {
-  return (
-    <div className="contenedor-presupuesto contenedor sombra">
-        <form className="formulario">
-            <div className="campo">
-                <label>Definir Presupuesto</label>
-                <input 
-                    className="nuevo-presupuesto"
-                    type="text"
-                    placeholder="Añade tu presupuesto"
-                />
-                
-            </div>
+import React from "react";
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 
-            <input 
-                type="submit" value="Añadir"
-            />
-        </form>
-    </div>
-  )
+const NuevoPresupuesto = ({
+    presupuesto, 
+    setPresupuesto, 
+    setIsValidPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if(!presupuesto || Number(presupuesto) < 0){
+            setMensaje("No es un presupuesto válido");
+            return;
+        }
+        setMensaje('')
+        setIsValidPresupuesto(true);
+        
+    }
+
+    return (
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form onSubmit={handlePresupuesto} className="formulario">
+                <div className="campo">
+                    <label>Definir Presupuesto</label>
+                    <input 
+                        className="nuevo-presupuesto"
+                        type="number"
+                        placeholder="Añade tu presupuesto"
+                        value={presupuesto}
+                        onChange={e => setPresupuesto(e.target.value)}
+                    />
+                    
+                </div>
+
+                <input 
+                    type="submit" value="Añadir"
+                />
+                {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+            </form>
+        </div>
+    )
 }
 export default NuevoPresupuesto
